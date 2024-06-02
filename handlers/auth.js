15e40df02fe9e96415c5da61e9a116a12c9b97e0
Sara Mauko <95652838+smauko@users.noreply.github.com>
2024-06-02 16:04:17 +0200

    signin gotov

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,6 +1,7 @@
 import mongo from "mongodb";
 import db from "../db/connection.js";
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 (   async()=>{
 await  db.collection("users").createIndex({Email: 1}, {unique: true});})();
@@ -26,5 +27,46 @@ export default{
             throw new Error("Korisnik već postoji ili je neka greska sa podacima");
             
         }
+    },
+
+    async logInUser(email, password) {
+        console.log("Body:", email, password);
+        let user = await db.collection('users').findOne({ Email: email });
+        console.log("u funkciji sam(podaci sa baze):", email, user.Password);
+        if (user && user.Password && (await bcrypt.compare(password, user.Password))) {
+            delete user.Password; 
+            let token = jwt.sign(user, process.env.JWT_SECRET, {
+                algorithm: 'HS512',
+                expiresIn: '1 week',
+            });
+            return {
+                token,
+                email: user.Email, 
+            };
+        } else {
+            throw new Error('Greška prilikom login-a');
+        }
+    },
+    verify(req, res, next){
+        if (req.headers['authorization']) {
+        let authorization = req.headers.authorization.split(" ");
+        let type = authorization[0];
+        let token = authorization[1];
+        try {
+        if(type !== 'Bearer'){
+            console.log('uso sam u try/if')
+            res.status(401).send();
+            return res.status(401).send();
+        }
+        else {
+            req.jwt = jwt.verify(token, process.env.JWT_SECRET);
+            return next();
+        }}catch (err) {
+            return res.status(401).send(); // HTTP not-authorized
+        }
+    } else {
+        return res.status(401).send(); // HTTP invalid request
+    }
+
     }
 }
--- index.js
+++ index.js
@@ -14,21 +14,37 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-
+router.route('/verify').get(auth.verify, (req,res)=>{
+    res.status(200).send('Potvrđen token korisnika: ' + req.jwt.Email);
+})
 
 router.route('/signup').post(async (req, res) => {
     let user = req.body;
     let email = req.body.email;
     try{
     let result = await auth.registerUser(user);
-    res.status(201).send("Korisnik uspješno kreiran sa mailom: " + email + "id:(" + result +")");}
+    res.status(201).send("Korisnik uspješno kreiran sa mailom: " + email + ",id:(" + result +")");}
     catch(e){
         res.status(500).send("Korisnik već postoji ili je greška sa podacima!");
     }
 });
 
-router.route('/login').get(async (req, res) => {
+router.route('/login').post(async (req, res) => {
+    let user = req.body;
+    let email = user.email;
+    let password = user.password;
+    console.log(user);
+    try {
+        console.log("u try sam")
+        let result = await auth.logInUser(email, password);
+        res.status(201).send(result);
+    } catch (e) {
+        console.log("catch")
+        res.status(501).json({
+            error: e.message,
+        });
     
+    }
 });
 
 app.listen(port, () => {
